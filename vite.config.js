b6c5909a9a5dbffc98f41c6dad7220bd5ee2e19a
xc <only_xingchen@163.com>
2022-07-07 18:12:32 +0800

    chore(vite.config): 新增 alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue2'
+
+function pathResolve(dir) {
+  return resolve(process.cwd(), '.', dir)
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      // @/xxxx => src/xxxx
+      '@': pathResolve('./src')
+    }
+  }
 })
